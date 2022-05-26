fc541bbfe72cf4079892d11c6cdea7866a535a2c
soorya.j <soorya.j@servicenow.com>
2022-05-26 18:52:30 +0530

    POST changes

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,5 +29,14 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public User retriveUser(@PathVariable int id){
 		return service.findOne(id);
+	}
+
+	//POST /users
+	//response code must be - CREATED
+	//input - details of user
+	//output - CREATED status & return the created URI
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user){
+		User savedUser = service.save(user);
 	}
 }
