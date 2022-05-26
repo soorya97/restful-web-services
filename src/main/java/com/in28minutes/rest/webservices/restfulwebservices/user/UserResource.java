0b47d106a7f2a2894a779d509f9028ddeff9cd3a
soorya.j <soorya.j@servicenow.com>
2022-05-26 19:45:01 +0530

    POST changes - return URI of created user

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -36,7 +39,11 @@ public class UserResource {
 	//input - details of user
 	//output - CREATED status & return the created URI
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user){
+	public ResponseEntity<Object> createUser(@RequestBody User user){
 		User savedUser = service.save(user);
+
+		// /user/{id} must be the URI of the newly created user
+		URI savedUserLocation = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getUserID()).toUri();
+		return ResponseEntity.created(savedUserLocation).build();
 	}
 }
